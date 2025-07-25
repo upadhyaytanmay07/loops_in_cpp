fd15fc30340e6a0b71a5237e74f00656ef8149da
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-07-25 17:20:17 +0530

    Create looping_SIT_5times.cpp

---
+++ looping_SIT_5times.cpp
@@ -0,0 +1,25 @@
+/* Tanmay Upadhyay
+PRN 24070123120
+ENTC B2 */
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=1; i<6; i++)
+    {
+        cout<<"SIT\n";
+    }
+}
+
+/* Output for this code
+
+SIT
+SIT
+SIT
+SIT
+SIT
+
+*/
