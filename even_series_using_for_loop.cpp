a2cd4b8e99c030e19c16886953083278cef58eaa
upadhyaytanmay07 <tanmayupadhyay1159@gmail.com>
2025-07-25 17:17:38 +0530

    Create even_series_using_for_loop.cpp

---
+++ even_series_using_for_loop.cpp
@@ -0,0 +1,29 @@
+/* Tanmay upadhyay
+entc b2
+PRN 24070123120 */
+
+
+// using loops
+
+ #include<iostream>
+ using namespace std;
+
+ int main()
+ {
+     int num;
+     for(num=0; num<11; num+=2)
+     {
+         cout<<num<<endl;
+     }
+ }
+
+/* Output for this code
+
+0
+2
+4
+6
+8
+10
+
+  */
